ceed9accc2fbcfa6159d1b29f836132b921a3c31
Samuel Elliott <samuel@fancy.org.uk>
2022-08-20 20:23:10 +0100

    Fix add button position

--- src/app/browser/components/icons/util.ts
+++ src/app/browser/components/icons/util.ts
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export const svg_styles: React.CSSProperties = {
-    height: '1em',
+    height: '1.2em',
     width: 'auto',
     fill: 'currentColor',
+    alignSelf: 'center',
+    verticalAlign: -4,
 };
