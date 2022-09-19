99423f263b818e31b16e53874950aca9515b4563
Samuel Elliott <samuel@fancy.org.uk>
2022-09-19 19:12:44 +0100

    Fix opening NookLink deep links

--- src/app/preload-webservice/quirks/nooklink.ts
+++ src/app/preload-webservice/quirks/nooklink.ts
@@ -14,6 +14,13 @@ if (webservice.id === NOOKLINK_WEBSERVICE_ID) {
     const url = new URL(location.href);
     const initurl = new URL(webserviceurl);
 
+    for (const key of [...url.searchParams.keys()]) {
+        if (!initurl.searchParams.has(key)) {
+            // Allow any extra query string parameters for deep links
+            url.searchParams.delete(key);
+        }
+    }
+
     debug('URL', url, initurl);
 
     if (url.origin === initurl.origin && url.href !== initurl.href) {
