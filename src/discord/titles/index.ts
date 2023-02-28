80f4d18ed7c58c45dd060588eca48240a69fe451
Samuel Elliott <samuel@fancy.org.uk>
2023-02-28 14:38:50 +0000

    Update Discord titles
    https://github.com/samuelthomas2774/nxapi/issues/55

--- src/discord/titles/index.ts
+++ src/discord/titles/index.ts
@@ -2,3 +2,4 @@ export * as nintendo from './nintendo.js';
 export * as mojang from './mojang.js';
 export * as capcom from './capcom.js';
 export * as the_pokemon_company from './the-pokémon-company.js';
+export * as thatgamecompany from './thatgamecompany.js';
