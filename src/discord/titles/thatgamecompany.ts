ccf0951df58ac1e8f6857ca036321eec01954626
Samuel Elliott <samuel@fancy.org.uk>
2023-02-28 14:38:50 +0000

    Update Discord titles
    https://github.com/samuelthomas2774/nxapi/issues/55

---
+++ src/discord/titles/thatgamecompany.ts
@@ -0,0 +1,11 @@
+import { Title } from '../types.js';
+
+export const titles: Title[] = [
+    {
+        // Sky: Children of the Light
+        id: '0100c52011460000',
+        client: '1080136394288144465',
+        showPlayingOnline: true,
+        showActiveEvent: true,
+    },
+];
