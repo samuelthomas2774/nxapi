10a6784fa7cf9c2403606de0c6c00c48fbf40f0b
Samuel Elliott <samuel@fancy.org.uk>
2023-07-12 23:04:32 +0100

    Update Discord titles
    https://github.com/samuelthomas2774/nxapi/issues/75

---
+++ src/discord/titles/epicgames.ts
@@ -0,0 +1,12 @@
+import { Title } from '../types.js';
+
+export const titles: Title[] = [
+    {
+        // Fall Guys
+        id: '0100c3c015738000',
+        client: '1128807730463911937',
+        showPlayingOnline: true,
+        showActiveEvent: true,
+        showDescription: false,
+    },
+];
--- src/discord/titles/index.ts
+++ src/discord/titles/index.ts
@@ -4,3 +4,4 @@ export * as capcom from './capcom.js';
 export * as the_pokemon_company from './the-pokémon-company.js';
 export * as thatgamecompany from './thatgamecompany.js';
 export * as concernedape from './concernedape.js';
+export * as epicgames from './epicgames.js';
